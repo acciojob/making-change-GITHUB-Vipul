b211cd5133c859e31e3d477e9be4f6ae4cb17a91
GITHUB-Vipul <126069867+GITHUB-Vipul@users.noreply.github.com>
2023-12-29 03:47:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const makeChange = (c) => {
   // your name here
+	let coins=[1,5,10,25];
+	let change=[0,0,0,0];
+	let i=coins.length-1;
+	while(c>=0&&i>=0)
+		{
+			console.log(c);
+			if(c-coins[i]>=0)
+			{
+				c-=coins[i];
+				change[i]++;
+			}
+			else {i--;}
+		}
+	let ans={q:change[3],d:change[2],n:change[1],p:change[0]};
+	return ans;
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
+const c =prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
